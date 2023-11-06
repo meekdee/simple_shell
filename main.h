5c7ef20433cd1a7f15ea5cc0707694c6bfc7e28f
meekdee <drewaju.16@gmail.com>
2023-11-06 13:23:34 -0800

    Handle Token

--- main.h
+++ main.h
@@ -4,7 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
+
 // Function declarations here
+char **handletoken(char *buffer);
+
 
 // Helping Functions here
 int _putchar(char c);
--- shell.c
+++ shell.c
@@ -1,12 +1,12 @@
-#include "shell.h"
+#include "main.h"
 
 /**
  * Few comments.
  *
 */
-int main(void)
+int main(int argc, char **argv)
 {
-	char *buffer = NULL;
+	char *buffer = NULL, **args;
 	size_t n = 0;
 	ssize_t read_n;
 
@@ -16,6 +16,7 @@ int main(void)
 		read_n = getline(&buffer, &n, stdin);
 		if (read_n == -1)
 			exit(EXIT_FAILURE);
+		args = handletoken(buffer);
 	}
 	return (0);
 }
