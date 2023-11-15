a52b9ba76bdf0a9e4524a2047182ee05a977647b
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2023-11-15 06:13:02 -0800

    refacted codes

--- display_terminal.c
+++
@@ -1,6 +0,0 @@
-#include "main.h"
-
-void display_prompt(void)
-{
-   _print("shell$$ ");
-}
--- main.h
+++ main.h
@@ -15,19 +15,4 @@ char *getevr(char *var, char **env);
 int _putchar(char c);
 void _prompt(void);
 
-/*
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <string.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-
-void display_terminal(void);
-void read_cmd(char *command,size_t size)
-void _print(const char *txt)
-void exec_cmd(const char *command)
-*/
 #endif
